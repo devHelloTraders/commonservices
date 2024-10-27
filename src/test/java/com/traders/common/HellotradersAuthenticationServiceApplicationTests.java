4c957d37f031b4a1cd35eecaf2e4d762afe0bf2d
bunty <bunty.hada36@yahoo.com>
2024-10-27 21:59:22 +0530

    Initial Commit

---
+++ src/test/java/com/traders/common/HellotradersAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traders.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotradersAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
